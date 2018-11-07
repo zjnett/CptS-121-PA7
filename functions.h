fe2fe9e3e5a3183bea058efa018c8b199c9d4ca7
Zach Nett <zachary.nett@wsu.edu>
2018-11-07 15:57:22 -0800

    Added main.c, functions.c, and functions.h files.

---
+++ functions.h
@@ -0,0 +1,12 @@
+/**************************************************
+Programmer: Zach Nett
+Class: CptS 121 Fall 2018
+Date: November 7 2018
+Purpose: An implementation of 5-Card-Draw Poker.
+***************************************************/
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#include <stdio.h>
+
+#endif
---
+++ main.c
@@ -0,0 +1,6 @@
+#include "functions.h"
+
+int main(void) {
+
+	return 0;
+}
