8e9fe17cb4d936caeb10aabf08c73c0e7a7563a3
Zach Nett <zachary.nett@wsu.edu>
2018-11-09 09:12:18 -0800

    Added Deitel & Deitel required starting code.

--- functions.c
+++ functions.c
@@ -1,1 +1,50 @@
 #include "functions.h"
+
+/* shuffle cards in deck */
+
+void shuffle(int wDeck[][13])
+{
+	int row = 0;    /* row number */
+	int column = 0; /*column number */
+	int card = 0;   /* card counter */
+
+					/* for each of the 52 cards, choose slot of deck randomly */
+	for (card = 1; card <= 52; card++)
+	{
+		/* choose new random location until unoccupied slot found */
+		do
+		{
+			row = rand() % 4;
+			column = rand() % 13;
+		} while (wDeck[row][column] != 0);
+
+		/* place card number in chosen slot of deck */
+		wDeck[row][column] = card;
+	}
+}
+
+/* deal cards in deck */
+void deal(const int wDeck[][13], const char *wFace[], const char *wSuit[])
+{
+	int row = 0;    /* row number */
+	int column = 0; /*column number */
+	int card = 0;   /* card counter */
+
+					/* deal each of the 52 cards */
+	for (card = 1; card <= 52; card++)
+	{
+		/* loop through rows of wDeck */
+		for (row = 0; row <= 3; row++)
+		{
+			/* loop through columns of wDeck for current row */
+			for (column = 0; column <= 12; column++)
+			{
+				/* if slot contains current card, display card */
+				if (wDeck[row][column] == card)
+				{
+					printf("%5s of %-8s%c", wFace[column], wSuit[row], card % 2 == 0 ? '\n' : '\t');
+				}
+			}
+		}
+	}
+}
--- functions.h
+++ functions.h
@@ -4,9 +4,14 @@ Class: CptS 121 Fall 2018
 Date: November 7 2018
 Purpose: An implementation of 5-Card-Draw Poker.
 ***************************************************/
+//Starting Code Authors: Deitel & Deitel - C How to Program
+//Starting Code copied from https://eecs.wsu.edu/~aofallon/cpts121/progassignments/main.c
 
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 #include <stdio.h>
+
+void shuffle(int wDeck[][13]);
+void deal(const int wDeck[][13], const char *wFace[], const char *wSuit[]);
 
 #endif
